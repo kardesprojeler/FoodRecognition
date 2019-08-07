f1185b8dc1c5b6ecb8292b1fdb17594f74c166b9
DurkanKaya <47315745+DurkanKaya@users.noreply.github.com>
2019-08-07 11:44:27 +0300

    modelde düzenleme yapıldı

--- Core/Model.py
+++ Core/Model.py
@@ -1,14 +1,32 @@
 import tensorflow as tf
 
+
 class Model(tf.keras.Model):
     def __init__(self):
         super(Model, self).__init__()
+        self.model_networks = {}
         self.model_layers = []
 
-    def add_layer(self, layer):
-        self.model_layers.append(layer)
+    def add_network(self, name=''):
+        if name not in self.model_networks:
+            self.model_networks[name] = []
+
+    def add_layer(self, layer, *network_names):
+        if len(self.model_networks) == 0:
+            self.add_network("main")
+
+        if len(network_names) == 0:
+            self.model_networks["main"].append(layer)
+        else:
+            for network_name in network_names:
+                if network_name in self.model_networks:
+                    self.model_networks[network_name].append(layer)
+                else:
+                    raise Exception('Layerin ekleneceği network bulunamadı!\nLayer Name:{}'.format(layer.name))
 
     def call(self, x: tf.Tensor, training: bool = True):
-        for layer in self.model_layers:
-            x = layer(x)
+        for network in self.model_networks:
+            for layer in network:
+                x = layer(x)
         return x
+
